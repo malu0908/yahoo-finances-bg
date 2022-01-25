a531f8cb4cfa1e0c318f29fcc371e42b338234c3
Maria <2018108015@bcc.unifal-mg.edu.br>
2022-01-25 15:04:01 -0300

    Att dag

--- airflow/dags/extractData.py
+++ airflow/dags/extractData.py
@@ -38,37 +38,7 @@ dag = DAG(
 
 # [START functions]
 def get_data_yahoo_finances():
-	tickers = [
-	  'AAPL',
-		'VALE',
-	]
-		
-	bucket_name = "yahoo-finances-bg"
-
-	session = boto3.Session(
-		aws_access_key_id=ACCESS_KEY,
-		aws_secret_access_key=SECRET_ACCESS
-	)
-	s3 = boto3.resource( 's3', aws_access_key_id=ACCESS_KEY, aws_secret_access_key=SECRET_ACCESS)
-	bucket_name = "yahoo-finances-bg"
-
-	for ticker in tickers:
-		url = 'https://query1.finance.yahoo.com/v8/finance/chart/' + ticker + '?interval=1h'
-		response = urlopen(url)
-
-		data_json = json.loads(response.read())
-		data_timestamp = data_json['chart']['result'][0]['timestamp']
-		data_json = data_json['chart']['result'][0]['indicators']['quote']
-		df = pd.DataFrame.from_dict(data_json[0])
-		df['timestamp'] = data_timestamp
-		df['ticker'] = ticker
-		df['year'] = datetime.now().year
-		df['month'] = datetime.now().month
-		df['day'] = datetime.now().day
-
-		bytes_to_write = df.to_csv(None, index=False).encode()
-		folder_name = f'inbound/{datetime.now().year}/{datetime.now().month}/{ticker}'
-		s3.Object(bucket_name=bucket_name, key=f'{folder_name}/finances_{ticker}_{datetime.now().day}.csv').put(Body=bytes_to_write)
+  print('a')
 # [END functions]
 
 extract_data_yahoo_finances = PythonOperator(
